77a88846aad8d90efa7ccba37b2919e9eac1ce41
frostant <975125701@qq.com>
2020-07-28 22:08:40 +0800

    12.6/12.7

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,10 +11,11 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien_Invation")
     
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while 1:
-        gf.check_events()  
+        gf.check_events(ship)  
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
