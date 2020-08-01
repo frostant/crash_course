101d9823ebae8df2512d63b1a56d9108026bea4b
frostant <975125701@qq.com>
2020-08-01 19:00:39 +0800

    13.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import sys
 from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -14,12 +15,13 @@ def run_game():
     
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    alien = Alien(ai_settings, screen)
 
     while 1:
         gf.check_events(ai_settings, screen, ship, bullets)  
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
